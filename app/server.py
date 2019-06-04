abfdcf4066f61add9cb006054e6f0e274dd8212d
JJ0131 <jxj362@case.edu>
2019-06-04 19:24:10 -0400

    update export file link and file name

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1D5-h7eRHAXOil6YcqiS7hel0u_GCED2b'
+export_file_name = 'computers-stage-2.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['desktop','laptop]
 path = Path(__file__).parent
 
 app = Starlette()
